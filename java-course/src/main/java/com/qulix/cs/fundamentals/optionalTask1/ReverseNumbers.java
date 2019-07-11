b91c6cb58f347879ec9343092b6b06c21008a34c
Degtyaryov Eugene <EDegtyaryov@qulix.com>
2019-07-11 11:21:07 +0300

    Fundamentals. Optional task 1. Пункт 2: вывод аргументов командной строки в обратном порядке

---
+++ java-course/src/main/java/com/qulix/cs/fundamentals/optionalTask1/ReverseNumbers.java
@@ -0,0 +1,11 @@
+package com.qulix.cs.fundamentals.optionalTask1;
+
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        if(args.length != 0) {
+            for(int i = args.length-1; i >= 0; i--) {
+                System.out.print(args[i] + " ");
+            }
+        }
+    }
+}
