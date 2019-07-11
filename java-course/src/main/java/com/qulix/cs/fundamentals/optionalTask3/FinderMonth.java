009e597108031002cfffd24e58f0d9b68a8bdf7e
Degtyaryov Eugene <EDegtyaryov@qulix.com>
2019-07-11 11:29:55 +0300

    Fundamentals. Optional task 3: вывод названия месяца по соответсвующему введенному порядковому номеру

---
+++ java-course/src/main/java/com/qulix/cs/fundamentals/optionalTask3/FinderMonth.java
@@ -0,0 +1,22 @@
+package com.qulix.cs.fundamentals.optionalTask3;
+
+import java.util.Scanner;
+
+public class FinderMonth {
+    public static void main(String[] args) {
+        int monthNumber;
+        Scanner scanner;
+
+        System.out.print("Input number of month: ");
+        scanner = new Scanner(System.in);
+        monthNumber = scanner.nextInt();
+        scanner.close();
+
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            System.out.println("Month - " + Months.values()[monthNumber - 1].toString().toLowerCase());
+        }
+        else {
+            System.out.println("You input invalid number of month.");
+        }
+    }
+}
---
+++ java-course/src/main/java/com/qulix/cs/fundamentals/optionalTask3/Months.java
@@ -0,0 +1,16 @@
+package com.qulix.cs.fundamentals.optionalTask3;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
