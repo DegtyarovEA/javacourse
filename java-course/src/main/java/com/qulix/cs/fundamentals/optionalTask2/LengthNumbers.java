06300918b0be15a383a0db6953dc87d1ac4baf54
Degtyaryov Eugene <EDegtyaryov@qulix.com>
2019-07-11 14:24:25 +0300

    Fundamentals. Optional task 2: - реализован метод (sortByLength) для сортировки списка чисел по возрастанию значения их длины

--- java-course/src/main/java/com/qulix/cs/fundamentals/optionalTask2/LengthNumbers.java
+++ java-course/src/main/java/com/qulix/cs/fundamentals/optionalTask2/LengthNumbers.java
@@ -8,12 +8,13 @@ public class LengthNumbers {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = readNumbers();
         System.out.println(minAndMaxLength(numbers));
+        System.out.println(sortByLength(numbers));
     }
 
     /**
      * Метод для поиска самого короткого и сомго длинного числа из заданного списка.
      */
-    public static String minAndMaxLength(ArrayList<Integer> array) {
+    private static String minAndMaxLength(ArrayList<Integer> array) {
         int minIndex = 0;
         int maxIndex = 0;
 
@@ -28,6 +29,28 @@ public class LengthNumbers {
 
         return "Num with max length: " + array.get(maxIndex) + ", length = " + array.get(maxIndex).toString().length() +
                 "\nNum with min length: " + array.get(minIndex) + ", length = " + array.get(minIndex).toString().length();
+    }
+
+    /**
+     * Метод для вывода чисел в порядке возрастания их длины.
+     */
+    private static String sortByLength(ArrayList<Integer> array) {
+        StringBuilder result = new StringBuilder();
+
+        while (!array.isEmpty()) {
+            int index = 0;
+            int minNumber = array.get(0);
+
+            for (int i = 0; i < array.size(); i++) {
+                if (array.get(i).toString().length() < Integer.toString(minNumber).length()) {
+                    minNumber = array.get(i);
+                    index = i;
+                }
+            }
+            result.append(minNumber).append(" ");
+            array.remove(index);
+        }
+        return result.toString();
     }
 
     /**
